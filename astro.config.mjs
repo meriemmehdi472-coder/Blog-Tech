29ee81b68be90984f22d35b737be4fb4f896df21
Meriem Cliking <meriem.mehdi@cliking.co>
2026-05-19 14:57:16 +0200

    Fix routing and deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-	site: 'https://meriemmehdi.github.io',
-	base: '/Blog-tech',
+  site: 'https://meriemmehdi472-coder.github.io',
+  base: isProd ? '/Blog-Tech' : '/',
 });
