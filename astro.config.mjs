3785277f6853561eb566b754135287f37f1e4e95
Meriem Cliking <meriem.mehdi@cliking.co>
2026-05-19 15:32:45 +0200

    test de foonctionnement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
   site: 'https://meriemmehdi472-coder.github.io',
-  base: isProd ? '/Blog-Tech' : '/',
+  base: '/Blog-Tech/',
 });
